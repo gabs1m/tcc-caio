9061378b9cea62480a11f8cd04bea71bb2749ab8
gabs1m <gabs1machado@gmail.com>
2024-09-21 18:48:50 -0300

    Refatoração dos cards do corpo da página inicial

--- index.php
+++ index.php
@@ -1,172 +1,92 @@
 <html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="index.css" />
+    <title>Menu Responsivo</title>
+  </head>
 
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="index.css" />
-  <title>Menu Responsivo</title>
-</head>
+  <body>
+    <header>
+      <nav class="nav-bar">
+        <div class="logo">
+          <img class="logo" src="imagens/logo.png" />
+        </div>
 
-<body>
-  <header>
-    <nav class="nav-bar">
-      <div class="logo">
-        <img class="logo" src="imagens/logo.png" />
-      </div>
-      <div class="nav-list">
+        <div class="nav-list">
+          <ul>
+            <li class="nav-item"><a href="#" class="nav-link">Esporte</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Cultura</a></li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Tecnologia</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Gastronomia</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="icon-barra-de-pesquisa">
+          <input
+            type="search"
+            class="icon-barra-de-pesquisa"
+            placeholder="busque aqui" />
+          <button class="lupa"><img src="imagens/lupa_icon.png" /></button>
+        </div>
+
+        <div class="login-button">
+          <button>
+            <a href="CrudUsuario/formsLogin.php"><img src="imagens/user_icon.png" /></a>
+          </button>
+        </div>
+
+        <div class="mobile-menu-icon">
+          <button onclick="menuShow()">
+            <img class="icon" src="imagens/icon_menu.png" />
+          </button>
+        </div>
+      </nav>
+
+      <div class="mobile-menu">
         <ul>
           <li class="nav-item"><a href="#" class="nav-link">Esporte</a></li>
           <li class="nav-item"><a href="#" class="nav-link">Cultura</a></li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">Tecnologia</a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">Gastronomia</a>
-          </li>
+          <li class="nav-item"><a href="#" class="nav-link">Tecnologia</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Gastronomia</a></li>
         </ul>
-      </div>
 
-      <div class="icon-barra-de-pesquisa">
-        <input
-          type="search"
-          class="icon-barra-de-pesquisa"
-          placeholder="busque aqui" />
-        <button class="lupa"><img src="imagens/lupa_icon.png" /></button>
+        <div class="login-button">
+          <button><a href="#">Entrar</a></button>
+        </div>
       </div>
+    </header>
 
-      <div class="login-button">
-        <button>
-          <a href="#"><img src="imagens/user_icon.png" /></a>
-        </button>
+    <div class="corpo">
+      <?php for($i = 0; $i < 10; $i++): ?>
+      <div class="card">
+        <img src="imagens/Nomes-para-cachorro_capa.png" />
+        <div>
+          <h1>Feira de Adoção</h1>
+          <h2>Evento disponível: Feira de adoção, 12/06/2024 - 16 da tarde</h2>
+          <button>Saiba mais</button>
+        </div>
       </div>
-
-      <div class="mobile-menu-icon">
-        <button onclick="menuShow()">
-          <img class="icon" src="imagens/icon_menu.png" />
-        </button>
-      </div>
-    </nav>
-
-    <div class="mobile-menu">
-      <ul>
-        <li class="nav-item"><a href="#" class="nav-link">Esporte</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Cultura</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Tecnologia</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Gastronomia</a></li>
-      </ul>
-
-      <div class="login-button">
-        <button><a href="#">Entrar</a></button>
-      </div>
-    </div>
-  </header>
-
-  <div class="corpo">
-    <div class="card">
-      <img src="img/Cópia de Nomes-para-cachorro_capa.png " />
-      <div>
-        <h1>Feira de Adoção</h1>
-        <h2>Evento disponível: Feira de adoção, 12/06/2024 - 16 da tarde</h2>
-        <button>Saiba mais</button>
-      </div>
+      <?php endfor; ?>
     </div>
 
-    <div class="card">
-      <img src="img/Cópia de Nomes-para-cachorro_capa.png " />
-      <div>
-        <h1>Feira de Adoção</h1>
-        <h2>Evento disponível: Feira de adoção, 12/06/2024 - 16 da tarde</h2>
-        <button>Saiba mais</button>
-      </div>
-    </div>
+    <footer>
+      <p>&copy; 2023 - Todos os direitos reservados</p>
+      <nav>
+        <ul>
+          <li><a href="#">Termos de uso</a></li>
+          <li><a href="#">Política de privacidade</a></li>
+          <li><a href="#">Sobre nós</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </nav>
+    </footer>
 
-    <div class="card">
-      <img src="img/Cópia de Nomes-para-cachorro_capa.png " />
-      <div>
-        <h1>Feira de Adoção</h1>
-        <h2>Evento disponível: Feira de adoção, 12/06/2024 - 16 da tarde</h2>
-        <button>Saiba mais</button>
-      </div>
-    </div>
-
-    <div class="card">
-      <img src="img/Cópia de Nomes-para-cachorro_capa.png " />
-      <div>
-        <h1>Feira de Adoção</h1>
-        <h2>Evento disponível: Feira de adoção, 12/06/2024 - 16 da tarde</h2>
-        <button>Saiba mais</button>
-      </div>
-    </div>
-
-    <div class="card">
-      <img src="img/Cópia de Nomes-para-cachorro_capa.png " />
-      <div>
-        <h1>Feira de Adoção</h1>
-        <h2>Evento disponível: Feira de adoção, 12/06/2024 - 16 da tarde</h2>
-        <button>Saiba mais</button>
-      </div>
-    </div>
-
-    <div class="card">
-      <img src="img/Cópia de Nomes-para-cachorro_capa.png " />
-      <div>
-        <h1>Feira de Adoção</h1>
-        <h2>Evento disponível: Feira de adoção, 12/06/2024 - 16 da tarde</h2>
-        <button>Saiba mais</button>
-      </div>
-    </div>
-
-    <div class="card">
-      <img src="img/Cópia de Nomes-para-cachorro_capa.png " />
-      <div>
-        <h1>Feira de Adoção</h1>
-        <h2>Evento disponível: Feira de adoção, 12/06/2024 - 16 da tarde</h2>
-        <button>Saiba mais</button>
-      </div>
-    </div>
-
-    <div class="card">
-      <img src="img/Cópia de Nomes-para-cachorro_capa.png " />
-      <div>
-        <h1>Feira de Adoção</h1>
-        <h2>Evento disponível: Feira de adoção, 12/06/2024 - 16 da tarde</h2>
-        <button>Saiba mais</button>
-      </div>
-    </div>
-
-    <div class="card">
-      <img src="img/Cópia de Nomes-para-cachorro_capa.png " />
-      <div>
-        <h1>Feira de Adoção</h1>
-        <h2>Evento disponível: Feira de adoção, 12/06/2024 - 16 da tarde</h2>
-        <button>Saiba mais</button>
-      </div>
-    </div>
-
-    <div class="card">
-      <img src="img/Cópia de Nomes-para-cachorro_capa.png " />
-      <div>
-        <h1>Feira de Adoção</h1>
-        <h2>Evento disponível: Feira de adoção, 12/06/2024 - 16 da tarde</h2>
-        <button>Saiba mais</button>
-      </div>
-    </div>
-  </div>
-
-  <footer>
-    <p>&copy; 2023 - Todos os direitos reservados</p>
-    <nav>
-      <ul>
-        <li><a href="#">Termos de uso</a></li>
-        <li><a href="#">Política de privacidade</a></li>
-        <li><a href="#">Sobre nós</a></li>
-        <li><a href="#">Contato</a></li>
-      </ul>
-    </nav>
-  </footer>
-
-  <script src="acao.js"></script>
-</body>
-
+    <script src="acao.js"></script>
+  </body>
 </html>
