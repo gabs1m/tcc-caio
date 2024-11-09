0753640273a1e5168dbce58debd84bbd42743158
gabs1m <gabs1machado@gmail.com>
2024-11-09 13:42:47 -0300

    Update vendor dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tcc/peacefun',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9061378b9cea62480a11f8cd04bea71bb2749ab8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tcc/peacefun' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9061378b9cea62480a11f8cd04bea71bb2749ab8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
